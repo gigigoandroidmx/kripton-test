534a8c319f83938ff9e14e9ec28766e4f1b04148
Juan Godinez Vera <juan.godinez@gigigo.com.mx>
2017-06-14 10:29:07 -0500

    Se quita funcionalidad de kmp para desacoplar la capa de presentación de domain

--- app/src/main/java/gigigo/com/kripton/HomeInteractor.java
+++ app/src/main/java/gigigo/com/kripton/HomeInteractor.java
@@ -1,22 +1,10 @@
 package gigigo.com.kripton;
 
-import gigigo.com.kmvp.IExecutor;
-import gigigo.com.kmvp.KInteractor;
-
 /**
  * @author Juan Godínez Vera - 5/2/2017.
  */
-public class HomeInteractor
-        extends KInteractor {
+public class HomeInteractor {
 
-    private String data;
-    private IExecutor executor;
-
-
-    @Override
-    public void run() {
-
-    }
 
     public String getMessage() {
         return "Hello Kripton 2.0!!!";
